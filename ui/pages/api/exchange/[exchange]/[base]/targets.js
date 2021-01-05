2f9ea99c47b4deadb05b989ec80a8f150659e9e6
Codegnosis <paul@codegnosis.co.uk>
2021-01-05 13:06:42 +0000

    ui - update API to use new ExchangePairs lookup table

--- ui/pages/api/exchange/[exchange]/[base]/targets.js
+++ ui/pages/api/exchange/[exchange]/[base]/targets.js
@@ -10,29 +10,43 @@ handler.get(async (req, res) => {
     query: { exchange, base },
   } = req
 
-  req.dbModels.CurrencyUpdates.findAll({
-    attributes: ["pairId"],
-    include: [
-      { model: req.dbModels.ExchangeOracles, attributes: ["id"], where: { exchange } },
-      {
-        model: req.dbModels.Pairs,
-        attributes: ["target"],
-        where: { base },
-        order: [["target", "ASC"]],
-      },
-    ],
-    group: ["ExchangeOracle.id", "CurrencyUpdates.pairId", "Pair.target"],
-    raw: true,
+  req.dbModels.ExchangeOracles.findOne({
+    attributes: ["id"],
+    where: { exchange },
   })
-    .then((data) => {
-      const dataReturn = []
-      for (let i = 0; i < data.length; i += 1) {
-        const target = data[i]["Pair.target"]
-        if (!dataReturn.includes(target)) {
-          dataReturn.push(target)
-        }
-      }
-      res.json(dataReturn.sort())
+    .then(function (exch) {
+      req.dbModels.ExchangePairs.findAll({
+        attributes: ["pairId"],
+        include: [
+          {
+            model: req.dbModels.Pairs,
+            attributes: ["target"],
+            group: ["Pair.target"],
+            where: { base },
+            order: [["target", "ASC"]],
+          },
+        ],
+        where: {
+          exchangeOracleId: exch.id,
+        },
+        raw: true,
+      })
+        .then((data) => {
+          const dataReturn = []
+          for (let i = 0; i < data.length; i += 1) {
+            const target = data[i]["Pair.target"]
+            if (!dataReturn.includes(target)) {
+              dataReturn.push(target)
+            }
+          }
+          res.json(dataReturn.sort())
+        })
+        .catch((err) => {
+          console.error(err)
+          res.status(500).send({
+            message: "error occurred while retrieving data.",
+          })
+        })
     })
     .catch((err) => {
       console.error(err)
