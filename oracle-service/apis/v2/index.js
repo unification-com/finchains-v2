57157ff0c0fd35869cc12183a1030d6bfc88202c
Codegnosis <paul@codegnosis.co.uk>
2021-07-29 13:29:40 +0100

    oracle-service - modify api v2 loader to load cex/dex exchange APIs

--- oracle-service/apis/v2/index.js
+++ oracle-service/apis/v2/index.js
@@ -2,19 +2,22 @@ require("dotenv").config()
 const fs = require("fs")
 const path = require("path")
 
-const basename = path.basename(__filename)
+const subdirs = ["cex", "dex"]
+const exclude = ["dexsubgraph.js", path.basename(__filename), "config.js"]
 
 const exchangeApis = {}
 
-fs.readdirSync(__dirname)
-  .filter((file) => {
-    return file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
-  })
-  .forEach((file) => {
-    const api = require(path.join(__dirname, file))
-    const name = file.replace(file.slice(-3), "")
-    exchangeApis[name] = api
-  })
+for (let i = 0; i < subdirs.length; i += 1) {
+  fs.readdirSync(`${__dirname}/${subdirs[i]}`)
+    .filter((file) => {
+      return file.indexOf(".") !== 0 && !exclude.includes(file) && file.slice(-3) === ".js"
+    })
+    .forEach((file) => {
+      const api = require(path.join(__dirname, subdirs[i], file))
+      const name = file.replace(file.slice(-3), "")
+      exchangeApis[name] = api
+    })
+}
 
 module.exports = {
   exchangeApis,
