45d3c0352d32faf06fa931e448edad696684597e
Codegnosis <paul@codegnosis.co.uk>
2021-08-03 15:08:53 +0100

    oracle-service - allow GraphQL queries to get all pairs in one query, or individual queries (fix for quickswap)

--- oracle-service/apis/v2/dex/dexsubgraph.js
+++ oracle-service/apis/v2/dex/dexsubgraph.js
@@ -3,7 +3,7 @@ const Web3 = require("web3")
 const { unwrapToken, trimDecimals, scientificToDecimal } = require("../../../utils")
 
 class DexSubgraph {
-  constructor(queryUrl, queryEndpoint, token0, token0Price, token1, token1Price) {
+  constructor(queryUrl, queryEndpoint, token0, token0Price, token1, token1Price, queryAtOnce = true) {
     this.queryEndpoint = queryEndpoint
     this.token0 = token0
     this.token0Price = token0Price
@@ -11,6 +11,7 @@ class DexSubgraph {
     this.token1Price = token1Price
 
     this.query = null
+    this.queryAtOnce = queryAtOnce
 
     this.client = new ApolloClient({
       link: createHttpLink({
@@ -20,10 +21,11 @@ class DexSubgraph {
     })
   }
 
-  setGqlQuery(pairs) {
-    this.query = gql`
+  setGqlQuery(query) {
+    if (this.queryAtOnce) {
+      this.query = gql`
 {
- ${this.queryEndpoint}(where: { id_in: ${JSON.stringify(pairs)} }){
+ ${this.queryEndpoint}(where: { id_in: ${JSON.stringify( query )} }){
      ${this.token0} {
        symbol
      }
@@ -35,6 +37,22 @@ class DexSubgraph {
  }
 }
 `
+    } else {
+      this.query = gql`
+{
+ ${this.queryEndpoint}(id: "${query}" ){
+     ${this.token0} {
+       symbol
+     }
+     ${this.token1} {
+       symbol
+     }
+     ${this.token0Price}
+     ${this.token1Price}
+ }
+}
+`
+    }
   }
 
   static extractData(pairData, baseKey, targetKey, priceKey) {
@@ -55,15 +73,32 @@ class DexSubgraph {
     }
   }
 
-  async getData(pairsToQuery) {
+  async getData(query) {
     const pairs = []
-    this.setGqlQuery(pairsToQuery)
-    const retData = await this.client.query({ query: this.query, fetchPolicy: "no-cache" })
+    const pairData = []
 
-    for (let i = 0; i < retData.data[this.queryEndpoint].length; i += 1) {
-      const pairData = retData.data[this.queryEndpoint][i]
-      const td0 = DexSubgraph.extractData(pairData, this.token0, this.token1, this.token1Price)
-      const td1 = DexSubgraph.extractData(pairData, this.token1, this.token0, this.token0Price)
+    if (this.queryAtOnce) {
+      this.setGqlQuery(query)
+      const retData = await this.client.query({ query: this.query, fetchPolicy: "no-cache" })
+
+      for (let i = 0; i < retData.data[this.queryEndpoint].length; i += 1) {
+        pairData.push(retData.data[this.queryEndpoint][i])
+      }
+    } else {
+      const retAwaits = []
+      for (let i = 0; i < query.length; i += 1) {
+        this.setGqlQuery(query[i])
+        retAwaits.push(this.client.query({ query: this.query, fetchPolicy: "no-cache" }))
+      }
+      const retData = await Promise.all(retAwaits)
+      for (let i = 0; i < retData.length; i += 1) {
+        pairData.push(retData[i].data[this.queryEndpoint])
+      }
+    }
+
+    for (let i = 0; i < pairData.length; i += 1) {
+      const td0 = DexSubgraph.extractData(pairData[i], this.token0, this.token1, this.token1Price)
+      const td1 = DexSubgraph.extractData(pairData[i], this.token1, this.token0, this.token0Price)
       pairs.push(td0)
       pairs.push(td1)
     }
@@ -72,7 +107,7 @@ class DexSubgraph {
       if (pairs.length > 0) {
         resolve(pairs)
       } else {
-        reject(Error(`no data found for ${JSON.stringify(pairsToQuery)}`))
+        reject(Error(`no data found for ${JSON.stringify(query)}`))
       }
     })
   }
