6045c15a08f3bb2c0fbbba309b9b6949719f7c2b
Codegnosis <paul@codegnosis.co.uk>
2023-11-22 13:44:34 +0000

    oracle-services - add MEXC exchange

---
+++ oracle-service/apis/v2/cex/mexc.js
@@ -0,0 +1,82 @@
+require("dotenv").config()
+const _ = require("lodash/core")
+const Web3 = require("web3")
+const { scientificToDecimal, fetcher } = require("../../../utils")
+
+const filter = [
+  "ADA/BTC",
+  "ADA/USDT",
+  "ATOM/BTC",
+  "ATOM/USDT",
+  "BCH/BTC",
+  "BCH/USDT",
+  "BTC/USDC",
+  "BTC/USDT",
+  "DOT/USDT",
+  "EOS/BTC",
+  "EOS/USDT",
+  "ETH/BTC",
+  "ETH/USDT",
+  "LINK/BTC",
+  "LINK/USDT",
+  "LTC/BTC",
+  "LTC/USDT",
+  "NEO/BTC",
+  "NEO/USDT",
+  "XLM/BTC",
+  "XLM/USDT",
+  "XRP/BTC",
+  "XRP/USDT",
+  "BONE/USDT",
+  "SHIB/USDC",
+  "SHIB/USDT",
+]
+
+const getPairData = (pair) => {
+  const base = pair.split("/", 1)[0]
+  const target = pair.split("/", 2)[1]
+  const apiPairName = `${base}${target}`
+  return { apiPairName, pair, base, target }
+}
+
+const getPrices = async () => {
+  const final = []
+  try {
+    const url = "https://api.mexc.com/api/v3/ticker/24hr"
+
+    const response = await fetcher(url)
+
+    for (let i = 0; i < filter.length; i += 1) {
+      const pair = filter[i]
+      const pairData = getPairData(pair)
+      const base = pairData.base
+      const target = pairData.target
+
+      const d = _.find(response.json, function (o) {
+        return o.symbol === pairData.apiPairName
+      })
+
+      if (d) {
+        const price = scientificToDecimal(d.lastPrice).toString()
+        const priceInt = Web3.utils.toWei(price, "ether")
+        const timestamp = Math.floor(Date.now() / 1000)
+        const td = {
+          base,
+          target,
+          pair,
+          price,
+          priceInt,
+          timestamp,
+        }
+        final.push(td)
+      }
+    }
+  } catch (err) {
+    console.error(err)
+  }
+  return final
+}
+
+module.exports = {
+  getPrices,
+}
