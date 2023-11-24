4f084ddad6752953c5a0881af7eeadfaeef938c2
Codegnosis <paul@codegnosis.co.uk>
2023-11-24 11:26:09 +0000

    ui - only output exchanges used in prices API endpoint

--- ui/pages/api/prices/[base]/[target]/[time].js
+++ ui/pages/api/prices/[base]/[target]/[time].js
@@ -11,8 +11,6 @@ handler.get(async (req, res) => {
     query: { base, target, time },
   } = req
 
-  const exchanges = req.exchanges.cexs.concat(req.exchanges.dexs)
-
   const d = new Date()
   const ts = Math.floor(d / 1000)
 
@@ -55,7 +53,10 @@ handler.get(async (req, res) => {
 
   req.dbModels.CurrencyUpdates.findAll({
     attributes: ["priceRaw"],
-    include: [{ model: req.dbModels.Pairs, attributes: ["name"], where: { base, target } }],
+    include: [
+      { model: req.dbModels.Pairs, attributes: ["name"], where: { base, target } },
+      { model: req.dbModels.ExchangeOracles, attributes: ["exchange"] },
+    ],
     where: {
       timestamp: {
         [Op.gte]: tsQuery,
@@ -64,20 +65,25 @@ handler.get(async (req, res) => {
     order: [["timestamp", "DESC"]],
   })
     .then(function (cData) {
+      const exchangesUsed = []
+
       const dataRet = {
         base,
         target,
         time,
         pair: `${base}/${target}`,
-        exchanges,
         exchange_type: "both",
       }
 
       const dataSet = []
       for (let i = 0; i < cData.length; i += 1) {
         dataSet.push(Number(cData[i].priceRaw))
+        if (!exchangesUsed.includes(cData[i].ExchangeOracle.exchange)) {
+          exchangesUsed.push(cData[i].ExchangeOracle.exchange)
+        }
       }
       dataRet.prices = dataSet
+      dataRet.exchanges = exchangesUsed
       res.json(dataRet)
     })
     .catch((err) => {
--- ui/pages/api/prices/[base]/[target]/dex/index.js
+++ ui/pages/api/prices/[base]/[target]/dex/index.js
@@ -33,7 +33,10 @@ handler.get(async (req, res) => {
       }
       req.dbModels.CurrencyUpdates.findAll({
         attributes: ["priceRaw"],
-        include: [{ model: req.dbModels.Pairs, attributes: ["name"], where: { base, target } }],
+        include: [
+          { model: req.dbModels.Pairs, attributes: ["name"], where: { base, target } },
+          { model: req.dbModels.ExchangeOracles, attributes: ["exchange"] },
+        ],
         where: {
           timestamp: {
             [Op.gte]: lastHour,
@@ -45,20 +48,25 @@ handler.get(async (req, res) => {
         order: [["timestamp", "DESC"]],
       })
         .then(function (cData) {
+          const exchangesUsed = []
+
           const dataRet = {
             base,
             target,
             time: "1H",
             pair: `${base}/${target}`,
-            exchanges,
             exchange_type: "dex",
           }
 
           const dataSet = []
           for (let i = 0; i < cData.length; i += 1) {
             dataSet.push(Number(cData[i].priceRaw))
+            if (!exchangesUsed.includes(cData[i].ExchangeOracle.exchange)) {
+              exchangesUsed.push(cData[i].ExchangeOracle.exchange)
+            }
           }
           dataRet.prices = dataSet
+          dataRet.exchanges = exchangesUsed
           res.json(dataRet)
         })
         .catch((err) => {
